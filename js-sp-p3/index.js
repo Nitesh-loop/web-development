5c26cf33a3e7d4c4ce9277191fde4e2898bdbaff
Nitesh-loop <nitesh166k@gmail.com>
2025-10-29 23:43:52 +0530

    BaseketBall ScoreBorad: working on js added reset button

--- js-sp-p3/index.js
+++ js-sp-p3/index.js
@@ -4,15 +4,38 @@ document.getElementById("scoreHome-el") = homeNum
 document.getElementById("scoreGuest-el") = guestNum
 
 function plusOneH() {
-    addOne()
+    homeNum += 1
     document.getElementById("scoreHome-el").textContent = homeNum
 }
 
 function plusOneG() {
-    addOne()
-    document.getElementById("scoreGuest-el").textContent = homeNum
+    guestNum += 1
+    document.getElementById("scoreGuest-el").textContent = guestNum
 }
 
-function addOne() {
-    homeNum += 1
+function plusTwoH() {
+    homeNum += 2
+    document.getElementById("scoreHome-el").textContent = homeNum
+}
+
+function plusTwoG() {
+    guestNum += 2
+    document.getElementById("scoreGuest-el").textContent = guestNum
+}
+
+function plusThreeH() {
+    homeNum += 3
+    document.getElementById("scoreHome-el").textContent = homeNum
+}
+
+function plusThreeG() {
+    guestNum += 3
+    document.getElementById("scoreGuest-el").textContent = guestNum
+}
+
+function restBtn() {
+    homeNum = 0
+    guestNum = 0
+    document.getElementById("scoreHome-el").textContent = homeNum
+    document.getElementById("scoreGuest-el").textContent = guestNum
 }

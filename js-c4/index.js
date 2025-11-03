81f49be5630653e077619c3478a0d06f573e73ef
Nitesh-loop <nitesh166k@gmail.com>
2025-11-03 16:34:15 +0530

    Objects

---
+++ js-c4/index.js
@@ -0,0 +1,14 @@
+// objects - store data in-depth - composite / complex data type
+// key-valye pairs (for remembering)
+
+let course = {
+    title: "Learn CSS Grid for free",
+    lessons: 16,
+    creator: "Nick Saga",
+    length: 50,
+    level: 2,
+    isfree: true,
+    tags: ["html","CSS"]
+}
+
+console.log(course.tags)

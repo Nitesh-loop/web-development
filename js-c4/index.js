f30c07416a017d35bdb2bf141165ab78f6461943
Nitesh-loop <nitesh166k@gmail.com>
2025-11-07 21:20:34 +0530

    Black Jack: completed

--- js-c4/index.js
+++ js-c4/index.js
@@ -1,6 +1,7 @@
 // objects - store data in-depth - composite / complex data type
 // key-valye pairs (for remembering)
 
+
 let course = {
     title: "Learn CSS Grid for free",
     lessons: 16,
@@ -12,3 +13,16 @@ let course = {
 }
 
 console.log(course.tags)
+
+console.log(course["tags"])     //bracket notation, just to rember: give you the same result as course.tags
+
+
+let airbnb = {
+    isOpen: false,
+    castleName: "Nick's Castle",
+    availableRooms: 10,
+    roomType: ["noraml", "delux", "villa"]
+} 
+
+
+
--- js-p3/index.js
+++ js-p3/index.js
@@ -9,20 +9,34 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Nick",
+    chips: 155,
+    // sayHello: function() {                                   //Anynomous fuction
+    //     console.log("hellooooo!!!")          
+    // }
+}
+
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+
 function getRandomCard() {
-    let randomNum = Math.floor( Math.random() * 13) + 1
+  
     if (randomNum === 1) {
         randomNum = 11
         return randomNum
     }
-    else if (randomNum > 10){
+    else if (randomNum > 10) {
         randomNum = 10
         return randomNum
     }
     else {
         return randomNum
     }
-    
+
 }
 
 function startGame() {
@@ -39,14 +53,14 @@ function renderGame() {
     // cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     cardsEl.textContent = "Cards: "
 
-    for (let i=0; i < cards.length; i++) {
+    for (let i = 0; i < cards.length; i++) {
         // cardsEl.textContent = " "
         cardsEl.textContent += cards[i] + " "
     }
 
     sumEl.textContent = "Sum: " + sum
 
-    if (sum <= 20 ) {
+    if (sum <= 20) {
         message = "Do you want to draw a new card? :)"
     }
     else if (sum === 21) {
@@ -64,11 +78,15 @@ function renderGame() {
 
 
 function newCard() {
-    let card = getRandomCard()
-    message = "New Card"
-    messageEl.textContent = message
-    sum += card
-    cards.push(card)
-    // console.log(cards)
-    renderGame()
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        message = "New Card"
+        messageEl.textContent = message
+        sum += card
+        cards.push(card)
+        // console.log(cards)
+        renderGame()
+    }
+
 }
